edcbe2dd4bf5d7f0db038afc838269aee536b87f
Victor Hugo Batista <vct.bta@gmail.com>
2019-11-26 08:58:15 -0300

    Change Routes component from functional to object oriented

--- src/Routes.js
+++ src/Routes.js
@@ -3,21 +3,25 @@ import Home from './pages/home/Home';
 import OtherPage from './pages/other-page/OtherPage';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const Routes = () => (
-  <BrowserRouter>
-    <nav>
-        <ul>
-        <li>
-            <Link to="/">Home</Link>
-        </li>
-        <li>
-            <Link to="/other-page">Other Page</Link>
-        </li>
-        </ul>
-    </nav>
-    <Route path="/" exact="true" component={Home} />
-    <Route path="/other-page" exact="true" component={OtherPage} />
-  </BrowserRouter>
-);
+class Routes extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <nav>
+            <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/other-page">Other Page</Link>
+            </li>
+            </ul>
+        </nav>
+        <Route path="/" exact="true" component={Home} />
+        <Route path="/other-page" exact="true" component={OtherPage} />
+      </BrowserRouter>
+    )
+  }
+}
 
 export default Routes;
